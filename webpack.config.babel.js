e79f6fe6ccf616b550b07d5a70ac73dad7f1c5af
karatechops <contact@lazers.tv>
2017-08-07 14:32:45 -0400

    ✨ Add Webpack 3.4.1

---
+++ webpack.config.babel.js
@@ -0,0 +1,119 @@
+import path from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const env = process.env.NODE_ENV || 'production';
+
+let plugins = [
+  new CopyWebpackPlugin([{ from: './public' }]),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(env)
+    }
+  })
+];
+
+const loaderOptionsConfig = {
+  options: {
+    sassLoader: {
+      includePaths: [
+        './node_modules'
+      ]
+    }
+  }
+};
+
+const devConfig = {};
+if (env === 'production') {
+  loaderOptionsConfig.minimize = true;
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    })
+  );
+} else {
+  plugins = plugins.concat([
+    new webpack.HotModuleReplacementPlugin()
+  ]);
+  devConfig.devtool = 'cheap-module-source-map';
+  devConfig.entry = [
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    './src/index.js'
+  ];
+  devConfig.devServer = {
+    compress: true,
+    clientLogLevel: 'none',
+    contentBase: path.resolve('./dist'),
+    publicPath: '/',
+    quiet: true,
+    hot: true,
+    watchOptions: {
+      ignored: /node_modules/
+    },
+    historyApiFallback: true,
+    proxy: {
+      '/api/*': 'http://localhost:8102'
+    }
+  };
+}
+
+plugins.push(new webpack.LoaderOptionsPlugin(loaderOptionsConfig));
+
+export default Object.assign({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.scss', '.css', '.json']
+  },
+  plugins,
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'file-loader', options: { name: '[name].css' } },
+          { loader: 'sass-loader',
+            options: {
+              outputStyle: 'compressed',
+              includePaths: [
+                './node_modules'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}, devConfig);
